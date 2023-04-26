1de83e09c6210a726e8c687833fd7c397fabafd5
mario5698 <42937972+mario5698@users.noreply.github.com>
2023-04-26 11:32:24 +0200

    auto commit

--- Libft/sources/libft.h
+++ Libft/sources/libft.h
@@ -25,5 +25,5 @@ void		*ft_calloc(ft_size_t count, ft_size_t size);
 void		*ft_memmove(void *dest, const void *src, ft_size_t n);
 char		*ft_substr(char const *s, unsigned int start , ft_size_t len);
 char		*ft_strjoin(char const *s1, char const *s2);
-
+char		*ft_strtrim(char const *s1, char const *set);
 #endif
---
+++ Libft/srcs/ft_strtrim.c
@@ -0,0 +1,7 @@
+char * ft_strtrim(char const *s1, char const *set)
+{
+	char *local;
+
+	
+	return(local);
+}
