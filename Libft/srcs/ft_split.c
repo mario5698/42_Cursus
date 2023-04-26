8323dd099b3300fc9f51db62d02c4e6f342084d8
mario5698 <42937972+mario5698@users.noreply.github.com>
2023-04-26 18:50:28 +0200

    auto commit

--- Libft/srcs/ft_split.c
+++ Libft/srcs/ft_split.c
@@ -2,11 +2,32 @@
 #include <stdlib.h>
 #include<stdio.h>
 
+int nextc(char const *s1, char c, int position)
+{
+	while(s1[position]==c)
+	{
+		position++;
+	}
+	return(position);
+}
+
+
 char **ft_split(char const *s, char c)
 {
 	char	**local;
+	int position;
+	int i=0;
 	/*
 	usar substr para alocar los separadores 
 	*/
+	position=0;
+	local = (char **)malloc(sizeof(char **)*ft_strlen(s));
+	position =nextc(s, c, position);
+	while(i<4)
+	{
+		*local = ft_substr(s, 0, position);
+		*local++;
+		i++;
+	}
 	return(local);
 }
