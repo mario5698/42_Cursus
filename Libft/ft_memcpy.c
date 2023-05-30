cb5736d067b9244be6caced9a7135df506a3634f
mario5698 <42937972+mario5698@users.noreply.github.com>
2023-05-30 13:46:54 +0200

    auto-commit

--- Libft/ft_memcpy.c
+++ Libft/ft_memcpy.c
@@ -6,13 +6,13 @@
 /*   By: marancib <marancib@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/01 15:40:10 by marancib          #+#    #+#             */
-/*   Updated: 2023/05/01 15:40:10 by marancib         ###   ########.fr       */
+/*   Updated: 2023/05/30 12:42:41 by marancib         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"libft.h"
 
-void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
+void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
 	size_t	i;
 
